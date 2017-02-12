c4ca57cc3b659201f64f88fe31990d09b46e0e2c
ToKu <toku@gmail.com>
2017-02-13 00:49:17 +0100

    Fix resync. (DEFAULT_HEAD).

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2593,9 +2593,9 @@ void Com_Init(char *commandLine) {
 	Com_StartupVariable(NULL);
 	// get dedicated here for proper hunk megs initialization
 #ifdef DEDICATED
-	com_dedicated = Cvar_Get ("dedicated", "1", CVAR_ROM);
+	com_dedicated = Cvar_Get("dedicated", "1", CVAR_ROM);
 #else
-	com_dedicated = Cvar_Get ("dedicated", "0", CVAR_LATCH);
+	com_dedicated = Cvar_Get("dedicated", "0", CVAR_LATCH);
 	Cvar_CheckRange(com_dedicated, 0, 1, qtrue);
 #endif
 	// allocate the stack based hunk allocator
