317d0b91b06abf60adbde01df5a75e3f9c233796
ToKu <toku@gmail.com>
2018-12-22 11:54:27 +0100

    ioquake3 -> 3526.

--- code/renderergl2/tr_glsl.c
+++ code/renderergl2/tr_glsl.c
@@ -1096,6 +1096,10 @@ void GLSL_InitGPUShaders(void) {
 
 			if (r_cubeMapping->integer) {
 				Q_strcat(extradefines, 1024, "#define USE_CUBEMAP\n");
+
+				if (r_cubeMapping->integer == 2) {
+					Q_strcat(extradefines, 1024, "#define USE_BOX_CUBEMAP_PARALLAX\n");
+				}
 			} else if (r_deluxeSpecular->value > 0.000001f) {
 				Q_strcat(extradefines, 1024, va("#define r_deluxeSpecular %f\n", r_deluxeSpecular->value));
 			}
